14c0ebce56f00594e8d59a53c6994e5010eddf2c
mateusz9438 <mateusz9438@gmail.com>
2016-11-24 16:25:48 +0100

    Added Gyroscope

--- LocationApp/app/src/main/java/com/example/locationapp/MainActivity.java
+++ LocationApp/app/src/main/java/com/example/locationapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
@@ -17,33 +18,30 @@ import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListe
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.location.LocationServices;
-
+import android.hardware.SensorEvent;
 import java.text.CollationElementIterator;
 
 import static java.lang.Math.*;
 
 
-public class MainActivity extends AppCompatActivity implements ConnectionCallbacks,OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity implements ConnectionCallbacks,OnConnectionFailedListener,SensorEventListener {
+
 
     private TextView locationText;
+    private TextView gyroscopeTextView;
     private GoogleApiClient mGoogleApiClient;
     private SensorManager mSensorManager;
     private Sensor mSensor;
-    private SensorEvent mSensorEvent;
+    private final static double EPSILON = 0.00001;
+    private static final float NS2S = 1.0f / 1000000000.0f;
+    private final float[] deltaRotationVector = new float[4];
+    private float timestamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        GoogleApiClient client = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */,
-                        this /* OnConnectionFailedListener */)
-                .addApi(Drive.API)
-                .addScope(Drive.SCOPE_FILE)
-                .setAccountName("users.account.name@gmail.com")
-                .build();
 
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -54,12 +52,12 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         }
 
         locationText=(TextView) findViewById(R.id.locationText);
-
+        gyroscopeTextView=(TextView) findViewById(R.id.gyroscopeTextView) ;
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL );
 
     }
 
@@ -81,6 +79,12 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
     protected void onStop() {
         mGoogleApiClient.disconnect();
         super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 
     @Override
@@ -119,13 +123,14 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
     }
 
 
-    private static final float NS2S = 1.0f / 1000000000.0f;
-    private final float[] deltaRotationVector = new float[4]();
-    private float timestamp;
 
+    @Override
     public void onSensorChanged(SensorEvent event) {
-        // This timestep's delta rotation to be multiplied by the current rotation
+        // This time step's delta rotation to be multiplied by the current rotation
         // after computing it from the gyro sample data.
+
+
+
         if (timestamp != 0) {
             final float dT = (event.timestamp - timestamp) * NS2S;
             // Axis of the rotation sample, not normalized yet.
@@ -133,24 +138,22 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
             float axisY = event.values[1];
             float axisZ = event.values[2];
 
+            gyroscopeTextView.setText("Gyroscope: "+axisX+"\n"+axisY+"\n"+axisZ);
+
+
             // Calculate the angular speed of the sample
-            double omegaMagnitude = sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+            float omegaMagnitude = (float) sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
 
             // Normalize the rotation vector if it's big enough to get the axis
-            // (that is, EPSILON should represent your maximum allowable margin of error)
             if (omegaMagnitude > EPSILON) {
                 axisX /= omegaMagnitude;
                 axisY /= omegaMagnitude;
                 axisZ /= omegaMagnitude;
             }
 
-            // Integrate around this axis with the angular speed by the timestep
-            // in order to get a delta rotation from this sample over the timestep
-            // We will convert this axis-angle representation of the delta rotation
-            // into a quaternion before turning it into the rotation matrix.
-            double thetaOverTwo = omegaMagnitude * dT / 2.0f;
-            float sinThetaOverTwo = sin(thetaOverTwo);
-            float cosThetaOverTwo = cos(thetaOverTwo);
+            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+            float sinThetaOverTwo = (float) sin(thetaOverTwo);
+            float cosThetaOverTwo = (float) cos(thetaOverTwo);
             deltaRotationVector[0] = sinThetaOverTwo * axisX;
             deltaRotationVector[1] = sinThetaOverTwo * axisY;
             deltaRotationVector[2] = sinThetaOverTwo * axisZ;
@@ -160,10 +163,17 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         float[] deltaRotationMatrix = new float[9];
         SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
 
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
 
-}
+
 
 
 
